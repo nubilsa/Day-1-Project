3171a1440b51d7209ae79dfab9ae8d47a6cb037b
nubilsa <sandrinenubia@gmail.com>
2022-06-22 13:01:37 -0700

    complete

---
+++ main.py
@@ -0,0 +1,7 @@
+from unicodedata import name
+
+
+n = input("Hey there, what's your name? \n")
+city = input("Wnat is the name of the city you grew up in? \n")
+pet = input("What's your pet's name? \n")
+print("Hey " + n + " " + "your band name could be " + city + " " + pet)
