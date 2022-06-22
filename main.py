25c6f58458b94f3b911f48be6a36399449ff1773
nubilsa <sandrinenubia@gmail.com>
2022-06-22 13:03:46 -0700

    complete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from unicodedata import name
 
 
+print("Welcome to Band name Generator!")
 n = input("Hey there, what's your name? \n")
 city = input("Wnat is the name of the city you grew up in? \n")
 pet = input("What's your pet's name? \n")
